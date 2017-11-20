eb3d86253e4fca0a76146f8d6d0b3a1437076314
Alexander Kozhukhovsky <aliaksandr.kazhukhouski@gmail.com>
2017-11-20 11:12:56 +0300

    Class Constants -> Static Example

---
+++ php-oop/class-constants/static-example.php
@@ -0,0 +1,15 @@
+<?php
+class Foo
+{
+    // As of PHP 5.3.0
+    const BAR = <<<'EOT'
+bar
+EOT;
+    // As of PHP 5.3.0
+    const BAZ = <<<EOT
+baz
+EOT;
+}
+
+echo Foo::BAR . "\n";
+echo Foo::BAZ . "\n";
