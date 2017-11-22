06666aa03498346fdaafeefe40af148a74af155d
Alexander Kozhukhovsky <aliaksandr.kazhukhouski@gmail.com>
2017-11-22 12:58:26 +0300

    Traits -> Trait Example

---
+++ php-oop/traits/trait-example.php
@@ -0,0 +1,20 @@
+<?php
+trait ezcReflectionReturnInfo
+{
+    public function getReturnType()
+    { /*1*/}
+    public function getReturnDescription()
+    { /*2*/}
+}
+
+class ezcReflectionMethod extends ReflectionMethod
+{
+    use ezcReflectionReturnInfo;
+    /* ... */
+}
+
+class ezcReflectionFunction extends ReflectionFunction
+{
+    use ezcReflectionReturnInfo;
+    /* ... */
+}
