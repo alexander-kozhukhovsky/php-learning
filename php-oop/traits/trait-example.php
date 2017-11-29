0fa897c68f258c3312ee0c06a10c2da52c2a1f98
Alexander Kozhukhovsky <aliaksandr.kazhukhouski@gmail.com>
2017-11-29 16:17:38 +0300

    Traits -> Trait Example Extended

--- php-oop/traits/trait-example.php
+++ php-oop/traits/trait-example.php
@@ -1,20 +1,77 @@
 <?php
-trait ezcReflectionReturnInfo
+trait Log
 {
-    public function getReturnType()
-    { /*1*/}
-    public function getReturnDescription()
-    { /*2*/}
+    protected function log($msg)
+    {
+        echo "$msg\n";
+    }
 }
 
-class ezcReflectionMethod extends ReflectionMethod
+trait PrintLog
 {
-    use ezcReflectionReturnInfo;
-    /* ... */
+    public function printDate()
+    {
+        return strftime("%d.%m.%Y");
+    }
+    public function printTime()
+    {
+        return strftime("%R");
+    }
 }
 
-class ezcReflectionFunction extends ReflectionFunction
+interface WithConstruct
 {
-    use ezcReflectionReturnInfo;
-    /* ... */
+    public function __construct(bool $debug);
 }
+
+class BaseClass implements WithConstruct
+{
+    use Log, PrintLog;
+
+    private $parentness;
+    private $parentText = '';
+
+    public function __construct(bool $debug = false)
+    {
+        if ($debug) {
+            $this->parentness = class_parents($this);
+            if (!empty($this->parentness)) {
+                foreach ($this->parentness as $parent => $name) {
+                    $this->parentText .= " <= $name";
+                }
+            }
+            echo "Sent from '" . get_called_class() . $this->parentText . "'\n";
+            echo "PrintLog: " . self::printTime() . " " . self::printDate() . PHP_EOL;
+        }
+    }
+
+    public function save()
+    {
+        $this->log('Initialization...');
+    }
+
+    public function sayHello($who)
+    {
+        echo "Hi $who from " . get_called_class() . "!\n";
+    }
+}
+
+class MyBaseClass extends BaseClass
+{}
+
+class MyFirstClass extends MyBaseClass
+{}
+
+$baseClass = new BaseClass(1);
+$baseClass->save();
+$baseClass->sayHello('First');
+echo PHP_EOL;
+
+$myBaseClass = new MyBaseClass(1);
+$myBaseClass->save();
+$myBaseClass->sayHello('Second');
+echo PHP_EOL;
+
+(new MyFirstClass(true))->save();
+(new MyFirstClass(false))->sayHello('Mr. Anderson');
+echo PHP_EOL;
