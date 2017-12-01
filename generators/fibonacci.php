3af55ca1efa0e07b6c43c8e7a2d9ef7f2746f129
Alexander Kozhukhovsky <aliaksandr.kazhukhouski@gmail.com>
2017-12-01 15:12:02 +0300

    fibonacci function changed to output key => value

--- generators/fibonacci.php
+++ generators/fibonacci.php
@@ -3,17 +3,17 @@ function fibonacci($position = 1)
 {
     $last = 0;
     $current = 1;
-    yield 1; // without "=" !!!
     $i = 0;
+    yield $i => 1; // changed for array output
     while ($i < $position) {
         $current = $last + $current;
         $last = $current - $last;
-        yield $current;
         $i++;
+        yield $i => $current; // changed for array output
     }
     return; // will exit generator
 }
 
-foreach (fibonacci(22) as $value) {
-    echo "$value\n";
+foreach (fibonacci(22) as $key => $value) { // changed for array output
+    echo "{$key} => {$value}\n";
 }
